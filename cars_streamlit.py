7da35a006365b0cc3d5956ecc91ac2b46a8105da
Steven <stevendruesne@gmail.com>
2021-12-10 16:00:23 +0100

    Finished form and adding functionality of new document to dataset

--- cars_streamlit.py
+++ cars_streamlit.py
@@ -1,3 +1,4 @@
+from os import execlp, execv
 from pathlib import Path
 import pandas as pd
 import streamlit as st
@@ -31,9 +32,17 @@ model = st.sidebar.selectbox('Search by model', cars_db.distinct('Model', {'Make
 car_to_print = cars_db.find({ "$and" : [{"Make": constructor}, {"Model": model}]}, {"_id":0, "Make":1, "Model":1, "Vehicle Style": 1, "Year": 1, "Engine HP": 1, "Engine Cylinders": 1, "Highway L/100km": 1, "City L/100km": 1 })
 
 for car in car_to_print:
-    st.write(f"La {car['Make']} {car['Model']} {car['Vehicle Style']} de {car['Year']} a {car['Engine HP']} chevaux et {car['Engine Cylinders']} cylindres. Sa consommation sur autoroute est de {car['Highway L/100km']} L au 100 km et de {car['City L/100km']} L au 100 km en ville.")
+    try :
+        st.write(f"La {car['Make']} {car['Model']} {car['Vehicle Style']} de {car['Year']} a {car['Engine HP']} chevaux et {car['Engine Cylinders']} cylindres. Sa consommation sur autoroute est de {car['Highway L/100km']} L au 100 km et de {car['City L/100km']} L au 100 km en ville.")
+    except :
+        st.write(f"La voiture {car['Make']} {car['Model']} ne fonctionne pas au pétrol raffiné")
+
+def add_car(make, model, year, hp, cyl):
+    cars_db.insert_one({'Make': make, 'Model': model, 'Year': int(year), 'Engine HP': int(hp), 'Engine Cylinders': int(cyl) })
+    st.write(f"La voiture de marque {make}, modèle {model}, de l'année {year}, de {hp} chevaux, de {cyl} cylindres, a été ajoutée à la base de donnée.")
     
-with st.sidebar.form(key='my_form'):
+
+with st.sidebar.form(key='my_form', clear_on_submit=True):
     st.write("Here you can add a new car to the list !")
     make_input = st.text_input(label="Enter the car's brand")
     model_input = st.text_input(label="Enter the car's model")
@@ -42,4 +51,6 @@ with st.sidebar.form(key='my_form'):
     cylinders_input = st.text_input(label="Enter the car's cylinders")
     submit_button = st.form_submit_button(label="Submit")
 
+if submit_button :
+    add_car(make_input, model_input, year_input, hp_input, cylinders_input)
 
